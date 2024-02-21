31a065dee7fd8a240fc4a215f4c5c3b9578a4a64
Tobiloba <salauoluwatobiloba0@gmail.com>
2024-02-21 22:05:35 +0100

    fixed padding error

--- frontend/src/components/common/loading/Loading.jsx
+++ frontend/src/components/common/loading/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center h-[100vh]">
       <div className="border bg-gray-500 p-2 rounded-full">
-        <div className="border bg-gray-200 p-6 rounded-full">
+        <div className="border bg-gray-200 p-4 rounded-full">
           <img src={LoadingGif} alt="loading" className="w-12 h-12" />
         </div>
       </div>
