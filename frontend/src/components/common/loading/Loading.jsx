b794886a9eafd5f633b72e71c094436c6e1595b8
Tobiloba <salauoluwatobiloba0@gmail.com>
2024-02-21 22:07:25 +0100

    fixed padding error

--- frontend/src/components/common/loading/Loading.jsx
+++ frontend/src/components/common/loading/Loading.jsx
@@ -4,7 +4,7 @@ import LoadingGif from '@/assets/lottie/loading.gif'
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      <div className="border bg-gray-500 p-2 rounded-full">
+      <div className="border bg-gray-500 p-1 rounded-full">
         <div className="border bg-gray-200 p-4 rounded-full">
           <img src={LoadingGif} alt="loading" className="w-12 h-12" />
         </div>
